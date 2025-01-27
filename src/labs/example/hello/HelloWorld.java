a644821ea5fcedb959f426707a52ed841608eac9
UnderratedTechW1z <daviscj899@mail.sandhills.edu>
2025-01-27 11:48:11 -0500

    This will be my first commit to the Java repository

---
+++ src/labs/example/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+@author: Christian Davis
+@date: 1/15/2025
+@purpose: My first JAVA program
+*/
+
+package labs.example.hello;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/labs/example/mammals/Mammal.java
@@ -0,0 +1,68 @@
+/*
+@author: Christian Davis
+@date: 1/15/2025
+@purpose: My first JAVA program
+*/
+
+package labs.example.mammals;
+//Class for methods and properties
+public class Mammal {
+    
+    // Properties of the Mammal Class
+    protected String hairColor;
+    protected String eyeColor;
+    protected String bodyTemp;
+    protected String fingerLength;
+    protected String height;
+    protected String bodyType;
+    protected String headSize;
+    public String name;
+
+    // Methods of the Mammal Class
+    public void mammal(String name) {
+        
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void run(){
+
+    }
+
+    public void eat(){
+        System.out.println("The " + this.name + "is eating!");
+    }
+
+    public void sleep(){
+
+    }
+
+    public void scratch(){
+
+    }
+
+    public void sit(){
+
+    }
+
+    public void climb(){
+
+    }
+
+    public void Throw(){
+
+    }
+
+    public void lick(){
+
+    }
+
+    public void read(){
+
+    }
+    
+}
+
+
