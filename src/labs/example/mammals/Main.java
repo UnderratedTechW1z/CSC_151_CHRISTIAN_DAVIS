0d015800f379e91bb9922634e7ac3e82419831a7
UnderratedTechW1z <daviscj899@mail.sandhills.edu>
2025-02-03 12:28:53 -0500

    Commit from 2/3/2025

--- src/labs/example/mammals/Main.java
+++ src/labs/example/mammals/Main.java
@@ -1,5 +1,18 @@
+/*
+@author: Christian Davis
+@date: 2/3/2025
+@purpose: My Main JAVA Program that uses the Mammal.Java file
+*/
 package labs.example.mammals;
 
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+        Mammal mammal = new Mammal();
+
+        mammal.setName("Dog");
+        mammal.getMammalDetails();
+        mammal.eat();
+        mammal.sit();
+        mammal.speak();
+    }
 }
--- src/labs/example/mammals/Mammal.java
+++ src/labs/example/mammals/Mammal.java
@@ -1,7 +1,7 @@
 /*
 @author: Christian Davis
-@date: 1/15/2025
-@purpose: My first JAVA program
+@date: 2/3/2025
+@purpose: My Mammal JAVA Program
 */
 
 package labs.example.mammals;
@@ -9,13 +9,13 @@ package labs.example.mammals;
 public class Mammal {
     
     // Properties of the Mammal Class
-    protected String hairColor;
-    protected String eyeColor;
-    protected String bodyTemp;
-    protected String fingerLength;
-    protected String height;
-    protected String bodyType;
-    protected String headSize;
+    protected String hairColor = ("Black");
+    protected String eyeColor = ("Brown");
+    protected String bodyTemp = ("Warm");
+    protected String fingerLength = ("Long");
+    protected String height = ("Tall");
+    protected String bodyType = ("Skinny");
+    protected String headSize = ("Medium");
     protected String name;
 
     // Methods of the Mammal Class
@@ -32,19 +32,25 @@ public class Mammal {
     }
 
     public void eat(){
-        System.out.println("The " + this.name + "is eating!");
+        System.out.println("The " + this.name + " is eating...");
     }
 
     public void sleep(){
-
+        System.out.println("The " + this.name + " is sleeping...");
     }
 
     public void scratch(){
-
+        System.out.println("The " + this.name + " scratched you, ouch...");
     }
 
-    public void sit(){
+    public void sit() throws Exception{
+        System.out.println("The " + this.name + " sat down...");
+        Thread.sleep(15000);
+        System.out.println("The " + this.name + " stood up...");
+    }
 
+    public void speak(){
+        System.out.println("The " + this.name + " barked!");
     }
 
     public void climb(){
@@ -61,6 +67,17 @@ public class Mammal {
 
     public void read(){
 
+    }
+
+    public void getMammalDetails(){
+        System.out.println("The " + this.name + " has the following properties: ");
+        System.out.println(hairColor + " Hair");
+        System.out.println(eyeColor + " Colored eyes");
+        System.out.println(bodyTemp + " body temperature");
+        System.out.println(fingerLength + " fingers");
+        System.out.println(height + " height");
+        System.out.println(bodyType + " body type");
+        System.out.println(headSize + " sized head");
     }
     
 }
