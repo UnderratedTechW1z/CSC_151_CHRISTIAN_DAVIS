a9c8775164bb8335f34f707f381eb38b9e68063a
SANDHILLS\LH218-21 <jad.talbert@gmail.com>
2025-03-03 11:50:40 -0500

    Commit for 3/3/2025 (Week 8 Arrays Start)

---
+++ src/labs/example/arrays/ArrayOperations.java
@@ -0,0 +1,28 @@
+/*
+@author: Christian Davis
+@date: 03/02/2025
+@purpose: The ArrayOperations.java program for CSC_151
+*/
+
+package labs.example.arrays;
+
+public class ArrayOperations
+{
+    public static int void createNewArray(int array_length)
+    {
+        int[]first_array = new int [array_length];
+        for (int x = 0; x < first_array; x++)
+        {
+            hours[x] = x;
+        }
+        private static int void displayArray(int[]first_array)
+        {
+            System.out.println("I create a new array and it now has " + array_length + " items in it.\n");
+            System.out.println("The array items and their values are listed below: ")
+            for (int index = 0; index < array_length; index++)
+            {
+                System.out.println(first_array[index]);
+            }
+        }
+    }
+}
---
+++ src/labs/example/arrays/Main.java
@@ -0,0 +1,7 @@
+/*
+@author: Christian Davis
+@date: 03/02/2025
+@purpose: The Main.java program for my arrays package in CSC_151
+*/
+
+package labs.example.arrays;
