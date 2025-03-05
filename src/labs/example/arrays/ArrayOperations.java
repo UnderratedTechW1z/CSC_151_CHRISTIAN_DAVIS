0321337a19ff3f723e3e24a52b8938c0126b78c4
UnderratedTechW1z <daviscj899@mail.sandhills.edu>
2025-03-05 12:17:10 -0500

    Commit from 3/5/2025 (Week 8 Lab Finished)

--- src/labs/example/arrays/ArrayOperations.java
+++ src/labs/example/arrays/ArrayOperations.java
@@ -1,6 +1,6 @@
 /*
 @author: Christian Davis
-@date: 03/02/2025
+@date: 03/05/2025
 @purpose: The ArrayOperations.java program for CSC_151
 */
 
@@ -8,21 +8,65 @@ package labs.example.arrays;
 
 public class ArrayOperations
 {
-    public static int void createNewArray(int array_length)
+    public void main(String[] args)
     {
-        int[]first_array = new int [array_length];
-        for (int x = 0; x < first_array; x++)
+        int[] arr = createNewArray(5);
+        
+        displayArray(arr);
+    }
+
+    public static int[] createNewArray(int array_length)
+    {
+        int[] first_array = new int [array_length];
+        for (int x = 0; x < first_array.length; x++)
         {
-            hours[x] = x;
+            first_array[x] = x + 1;
         }
-        private static int void displayArray(int[]first_array)
+        return first_array;
+    }
+    private static void displayArray(int[] first_array)
+    {
+        System.out.println("\nI created a new array and it now has " + first_array.length + " items in it.\n");
+        System.out.println("The array items and their values are listed below: ");
+        for (int index = 0; index < first_array.length; index++)
         {
-            System.out.println("I create a new array and it now has " + array_length + " items in it.\n");
-            System.out.println("The array items and their values are listed below: ")
-            for (int index = 0; index < array_length; index++)
+            System.out.println(first_array[index]);
+        }
+    }
+
+    public static int[] sortArray(int[] unsorted_Array)
+    {
+        int array_var;
+        for (int x = 0; x < unsorted_Array.length; x++)
+        {
+            for (int y = x + 1; y < unsorted_Array.length; y++)
             {
-                System.out.println(first_array[index]);
+                if (unsorted_Array[x] > unsorted_Array[y])
+                {
+                    array_var = unsorted_Array[x];
+                    unsorted_Array[x] = unsorted_Array[y];
+                    unsorted_Array[y] = array_var;
+                }
             }
         }
+        System.out.println("\nThe Sorted Array:");
+        for (int index = 0; index < unsorted_Array.length; index++)
+        {
+            System.out.println(unsorted_Array[index]);
+        }
+        return unsorted_Array;
+    }
+
+    public static void getDaysAndMonths()
+    {
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+        System.out.println("\nMonths of the Year with Days:\n");
+        for (int index = 0; index < months.length; index++)
+        {
+            System.out.println(months[index] + " has " + days[index] + " days.");
+        }
+        return;
     }
 }
--- src/labs/example/arrays/Main.java
+++ src/labs/example/arrays/Main.java
@@ -1,7 +1,20 @@
 /*
 @author: Christian Davis
-@date: 03/02/2025
+@date: 03/05/2025
 @purpose: The Main.java program for my arrays package in CSC_151
 */
 
 package labs.example.arrays;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayOperations arrayoperations = new ArrayOperations();
+        arrayoperations.main(args);
+
+        int[] unsorted_Array = {23, 7, 11, 16, 3, 39, 28, 5, 31, 45, 18, 41, 1, 33, 17, 22, 40, 9, 15, 13, 48, 25, 43, 37, 42, 10, 12, 27, 14, 38, 20, 46, 4, 50, 6, 30, 36, 32, 34, 49, 2, 35, 29, 44, 21, 8, 19, 47, 26, 24};
+        ArrayOperations.sortArray(unsorted_Array);
+        ArrayOperations.getDaysAndMonths();
+    }
+}
